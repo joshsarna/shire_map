86d1b7693f04cd5d55b3a47346981e93f458dad9
joshsarna <joshsarna@gmail.com>
2018-08-12 13:43:42 -0700

    make create action for locations

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -4,8 +4,29 @@ class Api::LocationsController < ApplicationController
     render "index.json.jbuilder"
   end
 
+  def create
+    @location = Location.new(
+      name: params[:name],
+      lat: params[:lat],
+      lng: params[:lng],
+      location_type: params[:location_type]
+    )
+    @location.save
+    render "show.json.jbuilder"
+  end
+
   def show
     @location = Location.find(params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    @location.update(
+      name: params[:name] || @location.name,
+      lat: params[:lat] || @location.lat,
+      lng: params[:lng] || @location.lng
+    )
     render "show.json.jbuilder"
   end
 end
