3b9c2945fc0c14fae13d5f0f3514fbfe60bdd6df
joshsarna <joshsarna@gmail.com>
2018-08-30 18:09:06 -0700

    generate Image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_235106) do
+ActiveRecord::Schema.define(version: 2018_08_31_010827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "image_url"
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
